176fbe5862abe8f02cf26f509647e67a9fa4b1bf
elif14 <warrenww14@gmail.com>
2023-10-22 19:02:12 -0400

    0.0.5: Work! Improving runtime maybe?

--- MPPython1/Solution.py
+++ MPPython1/Solution.py
@@ -21,18 +21,7 @@ class Solution:
         but they must remain within the Solution class.
         """
 
-        # print("isp id", self.isp)
-        # for i in self.graph:
-            # print("graph", i)
-        # for i in self.info["bandwidths"]:
-            # print(i)
-        # print(self.info["bandwidths"])
-
-        result = bfs_path(self.graph, self.isp, self.info["list_clients"])
-
-
-
-        paths, bandwidths, priorities = result, {}, {}
+        paths, bandwidths, priorities = bfs_path(self.graph, self.isp, self.info["list_clients"]), {}, {}
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
         return (paths, bandwidths, priorities)
