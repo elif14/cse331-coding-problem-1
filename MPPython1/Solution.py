9ad3cf91fddc509605053affe5d38e6dee15ff90
elif14 <warrenww14@gmail.com>
2023-10-22 18:37:51 -0400

    0.0.2: Trying bfs_path and some commented out for loops for print.

--- MPPython1/Solution.py
+++ MPPython1/Solution.py
@@ -20,6 +20,17 @@ class Solution:
         This method must be filled in by you. You may add other methods and subclasses as you see fit,
         but they must remain within the Solution class.
         """
+
+        # print("isp id", self.isp)
+        # for i in self.graph:
+            # print("graph", i)
+        # for i in self.info["bandwidths"]:
+            # print(i)
+        # print(self.info["bandwidths"])
+
+        bfs_path(self.graph, self.isp, self.info["list_clients"])
+
+
         paths, bandwidths, priorities = {}, {}, {}
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
